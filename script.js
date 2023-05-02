c59ad14fed6cdf008c53926e5700a5a8bd7f7078
camille <cmarca01@gmail.com>
2023-05-02 21:56:20 +0800

    2nd Commit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const toggleMenu = document.querySelector(".toggle__menu");
 const header = document.querySelector(".header")
-const headNav = document.querySelector(".header__nav ul");
+const headNav = document.querySelector(".header__nav2");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headNav.classList.toggle("open");
-  header.style="border-bottom: 1px solid #ffffff";
+  header.style="border-bottom: 1px solid #767676";
 });
